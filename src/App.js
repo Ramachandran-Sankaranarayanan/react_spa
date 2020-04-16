8766232f7d35d51967e32fd277234e7d53a10ce2
T G <sreeram.tg@cognizant.com>
2020-04-16 12:40:43 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch,Link } from 'react-router-dom';
+ 
+
+
+
+import Rider from "./Rider";
+import RiderList from "./riderlist";
+import Signin from "./signin";
+import Signup from "./signup";
+class App extends Component {
+  render() {
+    return (      
+       <BrowserRouter>
+        <div>
+        <h2>Home</h2>
+        <ul>
+  <li>
+    <Link to="/signin">Signin</Link>
+  </li>
+  <li>
+    <Link to="/signup">Signup</Link>
+  </li>
+  <li>
+    <Link to="/rider">Rider</Link>
+  </li>
+  <li>
+    <Link to="/riderList">RiderList</Link>
+  </li>
+</ul>
+           <Switch>                                     
+            <Route path="/rider">
+             <Rider />
+            </Route>
+            <Route path="/riderList">
+             <RiderList />
+             </Route>
+             <Route path="/signin">
+             <Signin />
+             </Route>
+             <Route path="/signup">
+             <Signup />
+             </Route>
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
+ 
+export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+ 
+import { NavLink } from 'react-router-dom';
+ 
+const Navigation = () => {
+    return (
+       <div>
+           <NavLink to="/rider">RiderForm</NavLink>
+           
+      </div>
+    );
+}
+ 
+export default Navigation;
