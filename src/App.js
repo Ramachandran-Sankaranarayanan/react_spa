e2b4b0266052f909548b1b9b41bee18cbcab423c
sanju s nair <sanju.nair4@cognizant.com>
2020-04-17 16:50:42 +0530

    API Integrated2

--- src/App.js
+++ src/App.js
@@ -7,27 +7,15 @@ import Rider from "./components/rider/Rider";
 import RiderList from "./components/rider/riderlist";
 import Signin from "./components/rider/signin";
 import Signup from "./components/rider/signup";
+import Navigation from './components/Navigation';
+
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
         <BrowserRouter>
           <div>
-            <h2>Home</h2>
-            <ul>
-              <li>
-                <Link to="/signin">Signin</Link>
-              </li>
-              <li>
-                <Link to="/signup">Signup</Link>
-              </li>
-              <li>
-                <Link to="/rider">Rider</Link>
-              </li>
-              <li>
-                <Link to="/riderList">RiderList</Link>
-              </li>
-            </ul>
+            <Navigation/>
             <Switch>
               <Route path="/rider">
                 <Rider />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,19 @@
 import React from 'react';
  
 import { NavLink } from 'react-router-dom';
+import './Navigation.css'
  
 const Navigation = () => {
     return (
-       <div>
-           <NavLink to="/rider">RiderForm</NavLink>
+       <div className="navbar-basic">
+        <div className="menus">
+            <NavLink to="/rider" className="menuItem">RiderForm</NavLink>
+            <NavLink to="/riderList" className="menuItem">Dasboard</NavLink>
+            <NavLink to="/signin" className="menuItem">signin</NavLink>
+            <NavLink to="/signup" className="menuItem">signup</NavLink>
+
+        </div>
+         
            
       </div>
     );
